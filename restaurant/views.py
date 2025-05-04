77e30d6341efc5889e52de81bda347590b368d4d
shiju <shijusivadazz86@gmail.com>
2025-05-04 23:27:29 +0530

    initial commit

---
+++ restaurant/views.py
@@ -0,0 +1,39 @@
+# from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from .forms import BookingForm
+from .models import Menu
+
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def book(request):
+    form = BookingForm()
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+    context = {'form':form}
+    return render(request, 'book.html', context)
+
+# Add your code here to create new views
+
+def menu(request):
+    menu=Menu.objects.all()
+    print(menu)
+    context = {'menu':menu}
+    return render(request,'menu.html',context)
+
+def display_menu_item(request,pk=None):
+    if pk:
+        menu_item = Menu.objects.get(pk=pk)
+    else:
+        menu_item=""
+
+    return render(request,'menu_item.html',{'menu_item':menu_item})
